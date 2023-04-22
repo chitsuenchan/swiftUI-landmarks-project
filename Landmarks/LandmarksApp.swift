a56e7e515b0d1e46deb2258f3e3f4f01d17b5b71
chi chan <chitsuenchan@gmail.com>
2023-04-22 22:17:02 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Chi  Chan on 22/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
